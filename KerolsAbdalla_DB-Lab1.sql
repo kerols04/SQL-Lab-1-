e5fa598b449c7a91ab9858001758ea7877f1d6df
kerols04 <116015613+kerols04@users.noreply.github.com>
2025-12-15 22:19:15 +0100

    Create KerolsAbdalla_DB-Lab1.sql

---
+++ KerolsAbdalla_DB-Lab1.sql
@@ -0,0 +1,33 @@
+/* DB-Lab1 - Samwail Abdalla
+   Databas: everyloop
+*/
+
+-- Uppgift 1
+GO
+
+-- Uppgift 2
+GO
+
+-- Uppgift 3
+GO
+
+-- Uppgift 4 (VG)
+GO
+
+-- Uppgift 5
+GO
+
+-- Uppgift 6
+GO
+
+-- Uppgift 7
+GO
+
+-- Uppgift 8
+GO
+
+-- Uppgift 9
+GO
+
+-- Uppgift 10 (VG)
+GO
